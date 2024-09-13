63b82310b3618140f19dfc249b77ec012f2585f0
bvv.21 <bvv.21@uni-dubna.ru>
2024-09-13 23:03:58 +0300

    2nd vd

---
+++ internal/app/store/userrepository.go
@@ -0,0 +1,32 @@
+package store
+
+import (
+	"github.com/BespalovVV/INKOT0/internal/app/model"
+)
+
+type Userrepository struct {
+	store *Store
+}
+
+func (r *Userrepository) Create(u *model.User) (*model.User, error) {
+	if err := r.store.db.QueryRow(
+		"INSERT INTO users(email, encrypted_password) VALUES ($1, $2) RETURNING id",
+		u.Email, u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+func (r *Userrepository) FindByEmail(email string) (*model.User, error) {
+	u := &model.User{}
+	if err := r.store.db.QueryRow("SELECT id, email, encrypted_password FROM users WHERE email = $1", email).Scan(
+		&u.ID,
+		&u.Email,
+		&u.EncryptedPassword,
+	); err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
