5944c6d30475157c0bb9451a3dcbe6be4d69a1d9
bvv.21 <bvv.21@uni-dubna.ru>
2024-09-15 20:27:12 +0300

    6ls

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -5,4 +5,5 @@ import "github.com/BespalovVV/INKOT0/internal/app/model"
 type UserRepository interface {
 	Create(*model.User) error
 	FindByEmail(string) (*model.User, error)
+	Find(int) (*model.User, error)
 }
